eb7540446c71c6dd2197d9f68b5bd6af839b0372
athinasalimm <127424862+athinasalimm@users.noreply.github.com>
2025-04-16 12:13:27 -0300

    loli

--- TP2-x86_64/ej1/ej1.asm
+++ TP2-x86_64/ej1/ej1.asm
@@ -61,7 +61,7 @@ string_proc_list_add_node_asm:
     movzx rcx, sil      
     mov r8, rdx         
 
-    mov rdi, cl
+    movzx rdi, cl
     mov rsi, r8
     call string_proc_node_create_asm
 
@@ -95,53 +95,46 @@ string_proc_list_add_node_asm:
     ret
 
 string_proc_list_concat_asm:
-    mov rbx, rdi         
-    movzx rcx, sil       
-    mov r8, rdx         
-
+    mov rbx, rdi     
+    movzx rcx, sil   
+    mov r8, rdx    
 
     mov rdi, 1
     call malloc
-    test rax, rax
-    je .return_null
+    mov byte [rax], 0     
+    mov r9, rax            
 
-    mov byte [rax], 0    
-    mov r9, rax          
-
-    mov r10, [rbx]       
+    mov r10, [rbx]        
 
 .loop:
     test r10, r10
-    je .concat_extra_hash
+    je .concat_extra_hash  
 
     movzx r11, byte [r10 + 16] 
     cmp r11b, cl
-    jne .skip_concat
+    jne .skip_concat           
 
-    mov rdi, r9
-    mov rsi, [r10 + 24]      
+    mov rdi, r9             
+    mov rsi, [r10 + 24]     
     call str_concat
+
 
     mov rdi, r9
     call free
 
-    mov r9, rax             
+
+    mov r9, rax
 
 .skip_concat:
-    mov r10, [r10 + 0]        
+    mov r10, [r10 + 0]
     jmp .loop
 
 .concat_extra_hash:
-    mov rdi, r9
-    mov rsi, r8            
+    mov rdi, r9        
+    mov rsi, r8       
     call str_concat
 
     mov rdi, r9
     call free
 
-    mov rax, r9              
-    ret
-
-.return_null:
-    xor rax, rax
     ret
