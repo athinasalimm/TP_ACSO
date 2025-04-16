6d37f49a5093b95064fa3319adb426a4000728f8
athinasalimm <127424862+athinasalimm@users.noreply.github.com>
2025-04-16 15:36:40 -0300

    LOL

--- TP2-x86_64/ej1/ej1.asm
+++ TP2-x86_64/ej1/ej1.asm
@@ -1,141 +1,161 @@
-; /* defines bool y puntero */
 %define NULL 0
 %define TRUE 1
 %define FALSE 0
 
-section .data
-
 section .text
+    global string_proc_list_create_asm
+    global string_proc_node_create_asm
+    global string_proc_list_add_node_asm
+    global string_proc_list_concat_asm
 
-global string_proc_list_create_asm
-global string_proc_node_create_asm
-global string_proc_list_add_node_asm
-global string_proc_list_concat_asm
-
-; FUNCIONES auxiliares que pueden llegar a necesitar:
-extern malloc
-extern free
-extern str_concat
+    extern malloc
+    extern free
+    extern strlen
+    extern strcpy
+    extern str_concat
 
 
 string_proc_list_create_asm:
-    mov rdi, 16         
-    call malloc          
-
-    test rax, rax       
-    je .return_null      
-
-    mov qword [rax], 0       
-
-    mov qword [rax + 8], 0   
-
-    ret
-
-.return_null:
-    xor rax, rax        
-    ret
+        push    rbp
+        mov     rbp, rsp
+        sub     rsp, 16
+        mov     edi, 16
+        call    malloc
+        mov     QWORD [rbp-8], rax
+        mov     rax, QWORD [rbp-8]
+        mov     QWORD [rax], 0
+        mov     rax, QWORD [rbp-8]
+        mov     QWORD [rax+8], 0
+        mov     rax, QWORD [rbp-8]
+        mov     rsp, rbp
+        pop     rbp
+        ret   
 
 string_proc_node_create_asm:
-    movzx rcx, dil       
-    mov rdx, rsi         
+        push    rbp
+        mov     rbp, rsp
+        sub     rsp, 32
+        mov     eax, edi
+        mov     QWORD [rbp-32], rsi
+        mov     BYTE  [rbp-20], al
+        mov     edi, 32
+        call    malloc
+        mov     QWORD [rbp-8], rax
+        mov     rax, QWORD [rbp-8]
+        movzx   edx, BYTE [rbp-20]
+        mov     BYTE  [rax+16], dl
+        mov     rax, QWORD [rbp-8]
+        mov     rdx, QWORD [rbp-32]
+        mov     QWORD [rax+24], rdx
+        mov     rax, QWORD [rbp-8]
+        mov     QWORD [rax], 0
+        mov     rax, QWORD [rbp-8]
+        mov     QWORD [rax+8], 0
+        mov     rax, QWORD [rbp-8]
+        mov     rsp, rbp
+        pop     rbp
+        ret
 
-    mov rdi, 32
-    call malloc
-
-    test rax, rax
-    je .return_null
-
-    mov qword [rax + 0], 0      
-    mov qword [rax + 8], 0      
-    mov byte  [rax + 16], cl   
-    mov qword [rax + 24], rdx   
-
-    ret
-
-.return_null:
-    xor rax, rax        
-    ret 
 
 string_proc_list_add_node_asm:
-    mov rbx, rdi        
-    movzx rcx, sil      
-    mov r8, rdx         
+        push    rbp
+        mov     rbp, rsp
+        sub     rsp, 48
+        mov     QWORD [rbp-24], rdi
+        mov     eax, esi
+        mov     QWORD [rbp-40], rdx
+        mov     BYTE  [rbp-28], al
+        movzx   eax, BYTE [rbp-28]
+        mov     rdx, QWORD [rbp-40]
+        mov     rsi, rdx
+        mov     edi, eax
+        call    string_proc_node_create_asm
+        mov     QWORD [rbp-8], rax
+        mov     rax, QWORD [rbp-24]
+        mov     rax, QWORD [rax]
+        test    rax, rax
+        jne     nodo_existente
+        mov     rax, QWORD [rbp-24]
+        mov     rdx, QWORD [rbp-8]
+        mov     QWORD [rax], rdx
+        mov     rax, QWORD [rbp-24]
+        mov     rdx, QWORD [rbp-8]
+        mov     QWORD [rax+8], rdx
+        jmp     fin_agregar
+nodo_existente:
+        mov     rax, QWORD [rbp-24]
+        mov     rax, QWORD [rax+8]
+        mov     rdx, QWORD [rbp-8]
+        mov     QWORD [rax], rdx
+        mov     rax, QWORD [rbp-24]
+        mov     rdx, QWORD [rax+8]
+        mov     rax, QWORD [rbp-8]
+        mov     QWORD [rax+8], rdx
+        mov     rax, QWORD [rbp-24]
+        mov     rdx, QWORD [rbp-8]
+        mov     QWORD [rax+8], rdx
+fin_agregar:
+        nop
+        mov     rsp, rbp
+        pop     rbp
+        ret
 
-    movzx rdi, cl
-    mov rsi, r8
-    call string_proc_node_create_asm
-
-    test rax, rax
-    je .fin          
-
-    mov r9, rax         
-
-    mov rax, [rbx]
-    mov rdx, [rbx + 8]
-
-    test rax, rax
-    jne .lista_no_vacia
-    test rdx, rdx
-    jne .lista_no_vacia
-
-    mov [rbx], r9       
-    mov [rbx + 8], r9   
-    jmp .fin
-
-.lista_no_vacia:
-    mov rax, [rbx + 8]
-
-    mov [rax + 0], r9
-
-    mov [r9 + 8], rax
-
-    mov [rbx + 8], r9
-
-.fin:
-    ret
 
 string_proc_list_concat_asm:
-    mov rbx, rdi         ; rbx ← lista
-    movzx rcx, sil       ; rcx ← type a filtrar
-    mov r8, rdx          ; r8 ← extra_hash (char*)
-
-    ; Reservamos espacio para un string vacío inicial
-    mov rdi, 1
-    call malloc
-    mov byte [rax], 0     ; string vacío → '\0'
-    mov r9, rax           ; r9 ← resultado parcial
-
-    mov r10, [rbx]        ; r10 ← primer nodo
-
-.loop:
-    test r10, r10
-    je .concat_extra_hash
-
-    movzx r11, byte [r10 + 16]   ; r11 ← nodo->type
-    cmp r11b, cl
-    jne .skip_concat             ; si no es del tipo deseado, salteamos
-
-    ; concatenamos r9 con nodo->hash
-    mov rdi, r9                  ; primer string
-    mov rsi, [r10 + 24]          ; segundo string (hash)
-    mov r12, r9                  ; salvamos r9 para liberarlo después
-    call str_concat              ; rax ← nuevo string concatenado
-    mov r9, rax
-    mov rdi, r12
-    call free
-
-.skip_concat:
-    mov r10, [r10 + 0]           ; siguiente nodo
-    jmp .loop
-
-.concat_extra_hash:
-    mov rdi, r9                  ; primer string (resultado parcial)
-    mov rsi, r8                  ; string adicional
-    mov r12, r9
-    call str_concat              ; concatenamos
-    mov r9, rax
-    mov rdi, r12
-    call free
-
-    mov rax, r9                  ; devolvemos el string final
-    ret
+        push    rbp
+        mov     rbp, rsp
+        sub     rsp, 64
+        mov     QWORD [rbp-40], rdi
+        mov     eax, esi
+        mov     QWORD [rbp-56], rdx
+        mov     BYTE  [rbp-44], al
+        mov     rax, QWORD [rbp-56]
+        mov     rdi, rax
+        call    strlen
+        add     rax, 1
+        mov     rdi, rax
+        call    malloc
+        mov     QWORD [rbp-8], rax
+        mov     rdx, QWORD [rbp-56]
+        mov     rax, QWORD [rbp-8]
+        mov     rsi, rdx
+        mov     rdi, rax
+        call    strcpy
+        mov     rax, QWORD [rbp-40]
+        mov     rax, QWORD [rax]
+        mov     QWORD [rbp-16], rax
+        jmp     loop_fin
+loop_cuerpo:
+        mov     rax, QWORD [rbp-16]
+        movzx   eax, BYTE [rax+16]
+        cmp     BYTE [rbp-44], al
+        jne     saltar_concat
+        mov     rax, QWORD [rbp-16]
+        mov     rdx, QWORD [rax+24]
+        mov     rax, QWORD [rbp-8]
+        mov     rsi, rdx
+        mov     rdi, rax
+        call    str_concat
+        mov     QWORD [rbp-24], rax
+        mov     rax, QWORD [rbp-8]
+        mov     rdi, rax
+        call    free
+        mov     rax, QWORD [rbp-24]
+        mov     QWORD [rbp-8], rax
+saltar_concat:
+        mov     rax, QWORD [rbp-16]
+        mov     rax, QWORD [rax]
+        mov     QWORD [rbp-16], rax
+loop_fin:
+        cmp     QWORD [rbp-16], 0
+        jne     loop_cuerpo
+        movzx   ecx, BYTE [rbp-44]
+        mov     rdx, QWORD [rbp-8]
+        mov     rax, QWORD [rbp-40]
+        mov     esi, ecx
+        mov     rdi, rax
+        call    string_proc_list_add_node_asm
+        mov     rax, QWORD [rbp-8]
+        mov     rsp, rbp
+        pop     rbp
+        ret
