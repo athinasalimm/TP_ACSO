878ded3d65f4b665b13730906e0adb97ecb2424d
athinasalimm <127424862+athinasalimm@users.noreply.github.com>
2025-04-16 16:21:28 -0300

    li

--- TP2-x86_64/ej1/ej1.asm
+++ TP2-x86_64/ej1/ej1.asm
@@ -104,45 +104,72 @@ string_proc_list_add_node_asm:
 ; ---------------------------------------------
 
 string_proc_list_concat_asm:
-    mov rbx, rdi     
-    movzx rcx, sil   
-    mov r8, rdx    
+    push    rbp
+    mov     rbp, rsp
+    sub     rsp, 64
 
-    mov rdi, 1
-    call malloc
-    mov byte [rax], 0     
-    mov r9, rax            
+    mov     [rbp-40], rdi         ; list
+    mov     [rbp-56], rdx         ; hash
+    mov     [rbp-44], sil         ; type
 
-    mov r10, [rbx]        
+    ; malloc(strlen(hash) + 1)
+    mov     rdi, [rbp-56]
+    call    strlen
+    add     rax, 1
+    mov     rdi, rax
+    call    malloc
+    mov     [rbp-8], rax
 
-.loop:
-    test r10, r10
-    je .concat_extra_hash  
+    ; strcpy(result, hash)
+    mov     rdi, [rbp-8]
+    mov     rsi, [rbp-56]
+    call    strcpy
 
-    movzx r11, byte [r10 + 16] 
-    cmp r11b, cl
-    jne .skip_concat           
+    ; current = list->first
+    mov     rax, [rbp-40]
+    mov     rax, [rax]
+    mov     [rbp-16], rax
 
-    mov rdi, r9             
-    mov rsi, [r10 + 24]     
-    call str_concat
+.loop_fin:
+    cmp     qword [rbp-16], 0
+    je      .fin_loop
 
+.loop_cuerpo:
+    mov     rax, [rbp-16]
+    movzx   eax, byte [rax+16]
+    cmp     byte [rbp-44], al
+    jne     .saltar_concat
 
-    mov rdi, r9
-    call free
+    ; temp = str_concat(result, current->hash)
+    mov     rax, [rbp-16]
+    mov     rsi, [rax+24]
+    mov     rdi, [rbp-8]
+    call    str_concat
+    mov     [rbp-24], rax
 
-    mov r9, rax
+    ; free(result)
+    mov     rdi, [rbp-8]
+    call    free
 
-.skip_concat:
-    mov r10, [r10 + 0]
-    jmp .loop
+    ; result = temp
+    mov     rax, [rbp-24]
+    mov     [rbp-8], rax
 
-.concat_extra_hash:
-    mov rdi, r9        
-    mov rsi, r8       
-    call str_concat
+.saltar_concat:
+    mov     rax, [rbp-16]
+    mov     rax, [rax]
+    mov     [rbp-16], rax
+    jmp     .loop_fin
 
-    mov rdi, r9
-    call free
+.fin_loop:
+    movzx   esi, byte [rbp-44]
+    mov     rdx, [rbp-8]
+    mov     rdi, [rbp-40]
+    call    string_proc_list_add_node_asm
 
+    mov     rax, [rbp-8]
+    mov     rsp, rbp
+    pop     rbp
     ret
+
+
