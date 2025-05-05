72520c04c2ec93bf6009a9c835027d01d5146957
athinasalimm <127424862+athinasalimm@users.noreply.github.com>
2025-05-05 15:26:49 -0300

    a

--- TP3-FileSystem/samples/sanity.py
+++ TP3-FileSystem/samples/sanity.py
@@ -10,7 +10,7 @@ def extract_filesys_inodes(str):
         if match:
             canonical.append(match)
     sorted_pairs = sorted(canonical, key=lambda line: int(line.group('inode')))
-    return '\n'.join([pair.expand('Inode \g<inode> checksum \g<chksum>') for pair in sorted_pairs])
+    return '\n'.join([pair.expand(r'Inode \g<inode> checksum \g<chksum>') for pair in sorted_pairs])
 
 # Filter for path check (CS110 assignment 1)
 def extract_filesys_paths(str):
@@ -18,6 +18,6 @@ def extract_filesys_paths(str):
     for line in str.split('\n'):
         match = re.match('Path [^ ]* (?P<path>[^ ]*) ([^ ]* ){5}(?P<chksum>[^ ]*)', line)
         if match:
-            canonical.append(match.expand('Path \g<path> checksum \g<chksum>'))
+            canonical.append(match.expand(r'Path \g<path> checksum \g<chksum>'))
     return '\n'.join(sorted(canonical))
 
