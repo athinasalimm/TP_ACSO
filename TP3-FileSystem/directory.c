aba3611b08cd82023428d9bb3c8b6faac658e584
athinasalimm <127424862+athinasalimm@users.noreply.github.com>
2025-05-05 15:15:40 -0300

    lol

--- TP3-FileSystem/directory.c
+++ TP3-FileSystem/directory.c
@@ -10,7 +10,35 @@
  * TODO
  */
 int directory_findname(struct unixfilesystem *fs, const char *name,
-		int dirinumber, struct direntv6 *dirEnt) {
-  //Implement your code here
-  return 0;
+  int dirinumber, struct direntv6 *dirEnt) {
+
+  struct inode dirinode;
+  if (inode_iget(fs, dirinumber, &dirinode) < 0)
+      return -1;
+
+  if ((dirinode.i_mode & IALLOC) == 0 || ((dirinode.i_mode & IFMT) != IFDIR)) {
+      fprintf(stderr, "directory_findname: inodo %d no es un directorio\n", dirinumber);
+      return -1;
+  }
+
+  int filesize = inode_getsize(&dirinode);
+  int blockCount = (filesize + DISKIMG_SECTOR_SIZE - 1) / DISKIMG_SECTOR_SIZE;
+
+  for (int i = 0; i < blockCount; i++) {
+      unsigned char buffer[DISKIMG_SECTOR_SIZE];
+      int bytes = file_getblock(fs, dirinumber, i, buffer);
+      if (bytes < 0) return -1;
+
+      int entries = bytes / sizeof(struct direntv6);
+      struct direntv6 *entry = (struct direntv6 *)buffer;
+
+      for (int j = 0; j < entries; j++) {
+          if (strncmp(entry[j].d_name, name, DIR_NAME_SIZE) == 0) {
+              *dirEnt = entry[j];  // copiar la entrada encontrada
+              return 0;
+          }
+      }
+  }
+
+  return -1;  // no se encontró
 }
--- TP3-FileSystem/direntv6.h
+++ TP3-FileSystem/direntv6.h
@@ -7,9 +7,11 @@
  * The Unix Version 6 code didn't use a structure like this but this is 
  * structure does match for format of a directory entry.
  */
+#define DIR_NAME_SIZE 14
+
 struct direntv6 {
   uint16_t d_inumber;   
-  char     d_name[14];
+  char     d_name[DIR_NAME_SIZE];
 };
 
 #endif // _DIRENTV6_H_
