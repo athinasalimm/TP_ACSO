f603a62641cd2c1f5d1b4a16f0c5e6e704a7e41c
athinasalimm <127424862+athinasalimm@users.noreply.github.com>
2025-05-05 14:59:17 -0300

    aaa

--- TP3-FileSystem/inode.c
+++ TP3-FileSystem/inode.c
@@ -9,8 +9,24 @@
  * TODO
  */
 int inode_iget(struct unixfilesystem *fs, int inumber, struct inode *inp) {
-    //Implement Code Here
-    return 0; 
+    if (inumber < 1) {
+        fprintf(stderr, "Error: inumber < 1\n");
+        return -1;
+    }
+
+    int sector = INODE_START_SECTOR + (inumber - 1) / 16;
+    unsigned char buffer[DISKIMG_SECTOR_SIZE];
+
+    if (diskimg_readsector(fs->dfd, sector, buffer) < 0) {
+        fprintf(stderr, "Error: couldn't read sector %d\n", sector);
+        return -1;
+    }
+
+    int index = (inumber - 1) % 16;
+    struct inode *inodes = (struct inode*) buffer;
+    *inp = inodes[index];  // copiamos el inodo leído a la salida
+
+    return 0;
 }
 
 /**
