d8d8ceb96ddb2aa3f784cd48804880df928e6166
athinasalimm <127424862+athinasalimm@users.noreply.github.com>
2025-05-05 15:17:15 -0300

    lol

--- TP3-FileSystem/pathname.c
+++ TP3-FileSystem/pathname.c
@@ -11,6 +11,23 @@
  * TODO
  */
 int pathname_lookup(struct unixfilesystem *fs, const char *pathname) {
-    //Implement code here
-	return 0;
+    if (pathname == NULL || pathname[0] != '/') return -1;
+
+    char pathcopy[1024];  // hacemos copia porque strtok modifica el string
+    strncpy(pathcopy, pathname, sizeof(pathcopy));
+    pathcopy[sizeof(pathcopy) - 1] = '\0';  // por seguridad
+
+    int inumber = 1;  // empezamos desde la raíz
+
+    char *token = strtok(pathcopy, "/");
+    while (token != NULL) {
+        struct direntv6 dirEntry;
+        if (directory_findname(fs, token, inumber, &dirEntry) < 0)
+            return -1;
+
+        inumber = dirEntry.d_inumber;
+        token = strtok(NULL, "/");
+    }
+
+    return inumber;
 }
