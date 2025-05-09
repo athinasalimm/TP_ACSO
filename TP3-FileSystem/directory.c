c535056758dd3246e9010ab2e960c767bead5530
athinasalimm <127424862+athinasalimm@users.noreply.github.com>
2025-05-09 13:54:38 -0300

    final_commit

--- TP3-FileSystem/directory.c
+++ TP3-FileSystem/directory.c
@@ -11,27 +11,21 @@
  */
 int directory_findname(struct unixfilesystem *fs, const char *name,
   int dirinumber, struct direntv6 *dirEnt) {
-
   struct inode dirinode;
   if (inode_iget(fs, dirinumber, &dirinode) < 0)
       return -1;
-
   if ((dirinode.i_mode & IALLOC) == 0 || ((dirinode.i_mode & IFMT) != IFDIR)) {
       fprintf(stderr, "directory_findname: inodo %d no es un directorio\n", dirinumber);
       return -1;
   }
-
   int filesize = inode_getsize(&dirinode);
   int blockCount = (filesize + DISKIMG_SECTOR_SIZE - 1) / DISKIMG_SECTOR_SIZE;
-
   for (int i = 0; i < blockCount; i++) {
       unsigned char buffer[DISKIMG_SECTOR_SIZE];
       int bytes = file_getblock(fs, dirinumber, i, buffer);
       if (bytes < 0) return -1;
-
       int entries = bytes / sizeof(struct direntv6);
       struct direntv6 *entry = (struct direntv6 *)buffer;
-
       for (int j = 0; j < entries; j++) {
           if (strncmp(entry[j].d_name, name, DIR_NAME_SIZE) == 0) {
               *dirEnt = entry[j];  
@@ -39,6 +33,5 @@ int directory_findname(struct unixfilesystem *fs, const char *name,
           }
       }
   }
-
   return -1;  
 }
--- TP3-FileSystem/file.c
+++ TP3-FileSystem/file.c
@@ -12,24 +12,16 @@ int file_getblock(struct unixfilesystem *fs, int inumber, int fileBlockNum, void
     struct inode in;
     if (inode_iget(fs, inumber, &in) < 0)
         return -1;
-
     if ((in.i_mode & IALLOC) == 0)
-    return -1;  // ❗ Inodo no asignado (archivo inexistente)
-
-
+    return -1; 
     int size = inode_getsize(&in);
     int totalBlocks = (size + DISKIMG_SECTOR_SIZE - 1) / DISKIMG_SECTOR_SIZE;
-
     if (fileBlockNum >= totalBlocks) return -1;
-
     int diskBlockNum = inode_indexlookup(fs, &in, fileBlockNum);
     if (diskBlockNum == -1) return -1;
-
     if (diskimg_readsector(fs->dfd, diskBlockNum, buf) < 0)
         return -1;
-
     int offset = fileBlockNum * DISKIMG_SECTOR_SIZE;
     int remaining = size - offset;
-
     return (remaining > DISKIMG_SECTOR_SIZE) ? DISKIMG_SECTOR_SIZE : remaining;
 }
